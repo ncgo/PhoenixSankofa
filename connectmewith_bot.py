3943dda94c4e86ceab044488d3c832d78f408784
jmsgrgry <67439567+jmsgrgry@users.noreply.github.com>
2020-08-01 12:31:53 -0500

    bot replies on tweets that mention blm
    bot can recognize any topic (first word following @ConnectWithMe_)

--- connectmewith_bot.py
+++ connectmewith_bot.py
@@ -14,6 +14,8 @@ api = tweepy.API(auth)
 
 FILE_NAME = 'last_seen_id.txt'
 
+#only looks at new tweets
+
 def retrieve_last_seen_id(file_name):
     f_read = open(file_name, 'r')
     last_seen_id = int(f_read.read().strip())
@@ -26,6 +28,7 @@ def store_last_seen_id(last_seen_id, file_name):
     f_write.close()
     return
 
+#analyses tweets
 
 def reply_to_tweets():
     print('retrieving tweets...', flush=True)
@@ -38,10 +41,19 @@ def reply_to_tweets():
         print(str(mention.id) + ' - ' + mention.full_text, flush=True)
         last_seen_id = mention.id
         store_last_seen_id(last_seen_id, FILE_NAME)
+
+#looks for any topic
+
+        print(mention.full_text.split(" ", 2)[1])
+
+#looks for BLM topic       
         
-        if 'blm' in mention.full_text.lower():
-            api.update_status('@' + mention.user.screen_name +
-                    'Preparing your connection...', mention.id)
+        if 'blm' in mention.full_text.lower().split(" ", 2)[1] or 'blacklivesmatter' in mention.full_text.lower().split(" ", 2)[1]:
+        	print('responding back...', flush=True)
+        	api.update_status('@' + mention.user.screen_name + ' Preparing your connection...', mention.id)
+        	print('update sent!', flush=True)
+
+#loops every 15sec
 
 while True:
     reply_to_tweets()

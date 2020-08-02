8ac01c7241de580726a04331a4b7749b33d98cda
nadia.co <ngarciaorozco@outlook.com>
2020-08-01 20:59:37 -0600

    Home

--- connectme/src/App.vue
+++ connectme/src/App.vue
@@ -1,56 +1,96 @@
 <template>
-  <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
+  <v-app style="font-family: 'Staatliches';">
+    <span>
+      <v-navigation-drawer
+        app
+        v-model="drawer"
+        class="lighten-2"
+        color="#1FA1F2"
+        dark
+        disable-resize-watcher
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
+        <v-list>
+          <template v-for="(item, index) in items">
+            <v-list-item :key="index">
+              <v-list-item-content>
+                {{ item.title }}
+              </v-list-item-content>
+            </v-list-item>
+            <v-divider :key="`divider-${index}`"></v-divider>
+          </template>
+        </v-list>
+      </v-navigation-drawer>
+      <v-app-bar color="#1FA1F2" dark fixed>
+        <v-app-bar-nav-icon class="hidden-md-and-up" @click="drawer = !drawer">
+        </v-app-bar-nav-icon>
+        <v-icon @click="$router.push('/')">mdi-link-variant</v-icon>
+        <v-toolbar-title class="mr-1 myTitle" @click="$router.push('/')">{{
+          appTitle
+        }}</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn
+          class="hidden-sm-and-down mr-3"
+          color="#E0193D"
+          @click="$router.push('/about')"
+          >ABOUT US
+        </v-btn>
+        <v-btn
+          class="hidden-sm-and-down mr-3"
+          color="#E0193D"
+          @click="$router.push('/data')"
+          >DATA
+        </v-btn>
+        <v-btn color="#6CCFF6 lighten-3" @click="tryItOut">TRY IT OUT</v-btn>
+      </v-app-bar>
+    </span>
     <v-main>
-      <HelloWorld />
+      <router-view></router-view>
     </v-main>
+    <v-card height="400px">
+      <v-footer padless fixed>
+        <v-card flat tile width="100%" class="blue lighten-1 text-center">
+          <v-card-text>
+            <v-btn v-for="icon in icons" :key="icon" class="mx-4" icon>
+              <v-icon size="24px" color="white">{{ icon }}</v-icon>
+            </v-btn>
+          </v-card-text>
+
+          <v-divider></v-divider>
+
+          <v-card-text class="white--text">
+            {{ new Date().getFullYear() }} —
+            <strong>Team Phoenix Sankofa</strong>
+          </v-card-text>
+        </v-card>
+      </v-footer>
+    </v-card>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
-
 export default {
   name: "App",
-
-  components: {
-    HelloWorld
+  data() {
+    return {
+      appTitle: "ConnectMeWith",
+      drawer: false,
+      items: [
+        { title: "MENU" },
+        { title: "ABOUT" },
+        { title: "MEET THE TEAM" }
+      ],
+      icons: ["mdi-home", "mdi-twitter", "mdi-twitter-retweet", "mdi-heart"],
+      padless: false,
+      variant: "fixed"
+    };
   },
-
-  data: () => ({
-    //
-  })
+  methods: {
+    tryItOut() {
+      window.open(
+        "https://twitter.com/intent/tweet?text=@ConnectMeWith_%20blm",
+        "_blank"
+      );
+    }
+  }
 };
 </script>

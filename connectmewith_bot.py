289f7d312ffd6a71eaa2a18289b1790ca9a93802
Aymen Dirar <63937200+adirar111@users.noreply.github.com>
2020-08-01 22:50:55 -0700

    working bot

--- connectmewith_bot.py
+++ connectmewith_bot.py
@@ -1,17 +1,11 @@
 import tweepy
 import time
+import random
 from tinydb import TinyDB, Query
+from access import api
+from direct_message import dm_article, dm_connection
 
 print("Connect Me With by Team Phoenix Sankofa for Twitter Hackathon")
-
-CONSUMER_KEY = "RFGhccptD6bbBwtuzcmqinsro"
-CONSUMER_SECRET = "vX5Vm4liCTmYqVL3cSdBhc2e6k7RpdNOl874ElXVaxnjbBcqup"
-ACCESS_KEY = "1289353192568958981-C3MN1FsmmUTt0LF5DQRct1REnxzSjq"
-ACCESS_SECRET = "yQoNOzSuaAXvnt1K1VgtVtJQwi1At28qGxCmE7E1AlBB8"
-
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
-api = tweepy.API(auth)
 
 FILE_NAME = "last_seen_id.txt"
 
@@ -42,8 +36,8 @@ def store_last_seen_id(last_seen_id, file_name):
 def reply_to_tweets_and_match():
     print("retrieving tweets...")
     last_seen_id = retrieve_last_seen_id(FILE_NAME)
-
     mentions = api.mentions_timeline(last_seen_id)
+
     for mention in reversed(mentions):
         print(str(mention.id) + " - " + mention.text)
         last_seen_id = mention.id
@@ -56,18 +50,31 @@ def reply_to_tweets_and_match():
         # stores the user ids currently in db in list to check for duplicate tweets
         id_list = []
         for entry in matching_db.search(Topic.topic == "blm"):
-            id_list.append(entry["id"])
+            id_list.append(entry["user_id"])
 
         print(id_list)
         print(mention.user.id in id_list)
 
         # looks for BLM topic
 
-        if "blm" in words[1] or "blacklivesmatter" in words[1]:
-            if len(matching_db.search(Topic.topic == "blm")) == 1:
+        if "blm" == words[1] or "blacklivesmatter" == words[1]:
+            print("blm topic")
+            if len(matching_db.search(Topic.topic == "blm")) == 0:
                 # if there's only one user
-                matching_db.insert({"topic": "blm", "id": mention.user.id})
+                matching_db.insert(
+                    {
+                        "name": mention.user.name,
+                        "handle": mention.user.screen_name,
+                        "user_id": mention.user.id_str,
+                        "topic": "blm",
+                        "open": 1,
+                        "location": mention.user.location,
+                    }
+                )
                 print("responding back...", flush=True)
+
+                # send them an article related to topic
+                dm_article(mention.user.id, mention.user.screen_name, "blm")
 
                 # try-except to avoid duplicate error
                 try:
@@ -87,8 +94,29 @@ def reply_to_tweets_and_match():
             elif (
                 mention.user.id not in id_list
             ):  # so it doesn't connect you with yourself
-                print("match made!")
-                matching_db.insert({"topic": "blm", "id": mention.user.id})
+                print("match can be made!")
+                # select a random user in the database before inserting this one:
+                current_users_in_db = matching_db.search(Topic.topic == "blm")
+                if len(matching_db.search(Topic.topic == "blm")) == 0:
+                    user_to_connect_index = 0
+                else:
+                    user_to_connect_index = random.randrange(
+                        0, len(current_users_in_db) - 1
+                    )
+                user_to_connect = current_users_in_db[user_to_connect_index]
+                user_to_connect_id = user_to_connect["user_id"]
+                user_to_connect_handle = user_to_connect["handle"]
+
+                matching_db.insert(
+                    {
+                        "name": mention.user.name,
+                        "handle": mention.user.screen_name,
+                        "user_id": mention.user.id_str,
+                        "topic": "blm",
+                        "open": 1,
+                        "location": mention.user.location,
+                    }
+                )
 
                 # try-except to avoid duplicate error
                 try:
@@ -103,8 +131,345 @@ def reply_to_tweets_and_match():
                     else:
                         raise error
 
-                print("preparing for a new match!")
-            # call the direct message/ text with those user
+                print("\nsending articles!\n")
+                dm_article(mention.user.id, mention.user.screen_name, "blm")
+                print("\npairing users!\n")
+                # dm this user about their connection
+                dm_connection(mention.user.id, user_to_connect_handle)
+                # dm the matched user
+                dm_connection(user_to_connect_id, mention.user.screen_name)
+                print("ready for a new match!")
+        # looks for acc topic
+        elif "acc" == words[1]:
+            print("acc topic")
+            if len(matching_db.search(Topic.topic == "acc")) == 0:
+                # if there's only one user
+                matching_db.insert(
+                    {
+                        "name": mention.user.name,
+                        "handle": mention.user.screen_name,
+                        "user_id": mention.user.id_str,
+                        "topic": "acc",
+                        "open": 1,
+                        "location": mention.user.location,
+                    }
+                )
+                print("responding back...", flush=True)
+
+                # send them an article related to topic
+                dm_article(mention.user.id, mention.user.screen_name, "acc")
+
+                # try-except to avoid duplicate error
+                try:
+                    api.update_status(
+                        "@"
+                        + mention.user.screen_name
+                        + " Preparing your connection...",
+                        mention.id,
+                    )
+                except tweepy.TweepError as error:
+                    if error.api_code == 187:
+                        # Do something special
+                        print("\n\nduplicate message\n\n")
+                    else:
+                        raise error
+                print("update sent!", flush=True)
+            elif (
+                mention.user.id not in id_list
+            ):  # so it doesn't connect you with yourself
+                print("match can be made!")
+                # select a random user in the database before inserting this one:
+                current_users_in_db = matching_db.search(Topic.topic == "acc")
+                if len(matching_db.search(Topic.topic == "acc")) == 0:
+                    user_to_connect_index = 0
+                else:
+                    user_to_connect_index = random.randrange(
+                        0, len(current_users_in_db) - 1
+                    )
+                user_to_connect = current_users_in_db[user_to_connect_index]
+                user_to_connect_id = user_to_connect["user_id"]
+                user_to_connect_handle = user_to_connect["handle"]
+
+                matching_db.insert(
+                    {
+                        "name": mention.user.name,
+                        "handle": mention.user.screen_name,
+                        "user_id": mention.user.id_str,
+                        "topic": "acc",
+                        "open": 1,
+                        "location": mention.user.location,
+                    }
+                )
+
+                # try-except to avoid duplicate error
+                try:
+                    api.update_status(
+                        "@" + mention.user.screen_name + " You have been connected!",
+                        mention.id,
+                    )
+                except tweepy.TweepError as error:
+                    if error.api_code == 187:
+                        # Do something special
+                        print("\n\nduplicate message\n\n")
+                    else:
+                        raise error
+
+                print("\nsending articles!\n")
+                dm_article(mention.user.id, mention.user.screen_name, "acc")
+                print("\npairing users!\n")
+                # dm this user about their connection
+                dm_connection(mention.user.id, user_to_connect_handle)
+                # dm the matched user
+                dm_connection(user_to_connect_id, mention.user.screen_name)
+                print("ready for a new match!")
+        elif "feminism" == words[1]:
+            print("feminism topic")
+            if len(matching_db.search(Topic.topic == "feminism")) == 0:
+                # if there's only one user
+                matching_db.insert(
+                    {
+                        "name": mention.user.name,
+                        "handle": mention.user.screen_name,
+                        "user_id": mention.user.id_str,
+                        "topic": "feminism",
+                        "open": 1,
+                        "location": mention.user.location,
+                    }
+                )
+                print("responding back...", flush=True)
+
+                # send them an article related to topic
+                dm_article(mention.user.id, mention.user.screen_name, "feminism")
+
+                # try-except to avoid duplicate error
+                try:
+                    api.update_status(
+                        "@"
+                        + mention.user.screen_name
+                        + " Preparing your connection...",
+                        mention.id,
+                    )
+                except tweepy.TweepError as error:
+                    if error.api_code == 187:
+                        # Do something special
+                        print("\n\nduplicate message\n\n")
+                    else:
+                        raise error
+                print("update sent!", flush=True)
+            elif (
+                mention.user.id not in id_list
+            ):  # so it doesn't connect you with yourself
+                print("match can be made!")
+                # select a random user in the database before inserting this one:
+                current_users_in_db = matching_db.search(Topic.topic == "feminism")
+                if len(matching_db.search(Topic.topic == "feminism")) == 0:
+                    user_to_connect_index = 0
+                else:
+                    user_to_connect_index = random.randrange(
+                        0, len(current_users_in_db) - 1
+                    )
+                user_to_connect = current_users_in_db[user_to_connect_index]
+                user_to_connect_id = user_to_connect["user_id"]
+                user_to_connect_handle = user_to_connect["handle"]
+
+                matching_db.insert(
+                    {
+                        "name": mention.user.name,
+                        "handle": mention.user.screen_name,
+                        "user_id": mention.user.id_str,
+                        "topic": "feminism",
+                        "open": 1,
+                        "location": mention.user.location,
+                    }
+                )
+
+                # try-except to avoid duplicate error
+                try:
+                    api.update_status(
+                        "@" + mention.user.screen_name + " You have been connected!",
+                        mention.id,
+                    )
+                except tweepy.TweepError as error:
+                    if error.api_code == 187:
+                        # Do something special
+                        print("\n\nduplicate message\n\n")
+                    else:
+                        raise error
+
+                print("\nsending articles!\n")
+                dm_article(mention.user.id, mention.user.screen_name, "feminism")
+                print("\npairing users!\n")
+                # dm this user about their connection
+                dm_connection(mention.user.id, user_to_connect_handle)
+                # dm the matched user
+                dm_connection(user_to_connect_id, mention.user.screen_name)
+                print("ready for a new match!")
+        elif "latinx" == words[1]:
+            print("latinx topic")
+            if len(matching_db.search(Topic.topic == "latinx")) == 0:
+                # if there's only one user
+                matching_db.insert(
+                    {
+                        "name": mention.user.name,
+                        "handle": mention.user.screen_name,
+                        "user_id": mention.user.id_str,
+                        "topic": "latinx",
+                        "open": 1,
+                        "location": mention.user.location,
+                    }
+                )
+                print("responding back...", flush=True)
+
+                # send them an article related to topic
+                dm_article(mention.user.id, mention.user.screen_name, "latinx")
+
+                # try-except to avoid duplicate error
+                try:
+                    api.update_status(
+                        "@"
+                        + mention.user.screen_name
+                        + " Preparing your connection...",
+                        mention.id,
+                    )
+                except tweepy.TweepError as error:
+                    if error.api_code == 187:
+                        # Do something special
+                        print("\n\nduplicate message\n\n")
+                    else:
+                        raise error
+                print("update sent!", flush=True)
+            elif (
+                mention.user.id not in id_list
+            ):  # so it doesn't connect you with yourself
+                print("match can be made!")
+                # select a random user in the database before inserting this one:
+                current_users_in_db = matching_db.search(Topic.topic == "latinx")
+                if len(matching_db.search(Topic.topic == "latinx")) == 0:
+                    user_to_connect_index = 0
+                else:
+                    user_to_connect_index = random.randrange(
+                        0, len(current_users_in_db) - 1
+                    )
+                user_to_connect = current_users_in_db[user_to_connect_index]
+                user_to_connect_id = user_to_connect["user_id"]
+                user_to_connect_handle = user_to_connect["handle"]
+
+                matching_db.insert(
+                    {
+                        "name": mention.user.name,
+                        "handle": mention.user.screen_name,
+                        "user_id": mention.user.id_str,
+                        "topic": "latinx",
+                        "open": 1,
+                        "location": mention.user.location,
+                    }
+                )
+
+                # try-except to avoid duplicate error
+                try:
+                    api.update_status(
+                        "@" + mention.user.screen_name + " You have been connected!",
+                        mention.id,
+                    )
+                except tweepy.TweepError as error:
+                    if error.api_code == 187:
+                        # Do something special
+                        print("\n\nduplicate message\n\n")
+                    else:
+                        raise error
+
+                print("\nsending articles!\n")
+                dm_article(mention.user.id, mention.user.screen_name, "latinx")
+                print("\npairing users!\n")
+                # dm this user about their connection
+                dm_connection(mention.user.id, user_to_connect_handle)
+                # dm the matched user
+                dm_connection(user_to_connect_id, mention.user.screen_name)
+                print("ready for a new match!")
+        else:
+            topic_submitted = words[1]
+            print("custom topic: " + topic_submitted)
+            print("latinx topic")
+            if len(matching_db.search(Topic.topic == topic_submitted)) == 0:
+                # if there's only one user
+                matching_db.insert(
+                    {
+                        "name": mention.user.name,
+                        "handle": mention.user.screen_name,
+                        "user_id": mention.user.id_str,
+                        "topic": topic_submitted,
+                        "open": 1,
+                        "location": mention.user.location,
+                    }
+                )
+                print("responding back...", flush=True)
+
+                # send them an article related to topic
+                dm_article(mention.user.id, mention.user.screen_name, topic_submitted)
+
+                # try-except to avoid duplicate error
+                try:
+                    api.update_status(
+                        "@"
+                        + mention.user.screen_name
+                        + " Preparing your connection...",
+                        mention.id,
+                    )
+                except tweepy.TweepError as error:
+                    if error.api_code == 187:
+                        # Do something special
+                        print("\n\nduplicate message\n\n")
+                    else:
+                        raise error
+                print("update sent!", flush=True)
+            elif (
+                mention.user.id not in id_list
+            ):  # so it doesn't connect you with yourself
+                print("match can be made!")
+                # select a random user in the database before inserting this one:
+                current_users_in_db = matching_db.search(Topic.topic == topic_submitted)
+                if len(matching_db.search(Topic.topic == topic_submitted)) == 0:
+                    user_to_connect_index = 0
+                else:
+                    user_to_connect_index = random.randrange(
+                        0, len(current_users_in_db) - 1
+                    )
+                user_to_connect = current_users_in_db[user_to_connect_index]
+                user_to_connect_id = user_to_connect["user_id"]
+                user_to_connect_handle = user_to_connect["handle"]
+
+                matching_db.insert(
+                    {
+                        "name": mention.user.name,
+                        "handle": mention.user.screen_name,
+                        "user_id": mention.user.id_str,
+                        "topic": topic_submitted,
+                        "open": 1,
+                        "location": mention.user.location,
+                    }
+                )
+
+                # try-except to avoid duplicate error
+                try:
+                    api.update_status(
+                        "@" + mention.user.screen_name + " You have been connected!",
+                        mention.id,
+                    )
+                except tweepy.TweepError as error:
+                    if error.api_code == 187:
+                        # Do something special
+                        print("\n\nduplicate message\n\n")
+                    else:
+                        raise error
+
+                print("\nsending articles!\n")
+                dm_article(mention.user.id, mention.user.screen_name, topic_submitted)
+                print("\npairing users!\n")
+                # dm this user about their connection
+                dm_connection(mention.user.id, user_to_connect_handle)
+                # dm the matched user
+                dm_connection(user_to_connect_id, mention.user.screen_name)
+                print("ready for a new match!")
 
     print(matching_db.all())
 
@@ -114,3 +479,8 @@ def reply_to_tweets_and_match():
 while True:
     reply_to_tweets_and_match()
     time.sleep(15)
+
+# for development
+# mentions = api.mentions_timeline()
+# for mention in reversed(mentions):
+#     print(mention.text, mention.user.screen_name, mention.user.id)
---
+++ direct_message.py
@@ -0,0 +1,44 @@
+from access import api
+from links import return_random_link
+import tweepy
+
+# dm link to user with article
+def dm_article(user_id, user_handle, topic):
+    link = return_random_link(topic)
+    # api.send_direct_message(user_id, "Hey, here's an article you might like!\n" + link)
+    try:
+        api.send_direct_message(
+            user_id,
+            "Hey "
+            + user_handle
+            + "! Here's some news related to "
+            + topic
+            + "!\n"
+            + link,
+        )
+
+    except tweepy.TweepError as error:
+        if error.api_code == 349:
+            api.update_status(
+                "@"
+                + user_handle
+                + " Please follow this account and try again so we can connect you via DMs!",
+                user_id,
+            )
+            api.create_friendship(user_id)
+        else:
+            print("something else went wrong")
+
+
+# dm user that they've been connected to another user\
+def dm_connection(user_id, connected_user_handle):
+    api.send_direct_message(
+        user_id,
+        "Hey! You and @"
+        + connected_user_handle
+        + " have shared similar interests! Hit them up and talk about your passions! Use one of the articles we sent as a conversation starter!",
+    )
+
+
+# dm_article(1266538686558044160, "rockets")
+# dm_connection(1266538686558044160, "KrisMadas")
